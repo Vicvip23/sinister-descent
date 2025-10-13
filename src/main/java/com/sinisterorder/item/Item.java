a3cc208cc3069c60d3c4f66d0219be1f0341f84f
Vicvip23 <wicekwiacek@gmail.com>
2025-10-13 18:56:38 +0200

    Fixed duped Item class definition and replaced direct access to Item property with getters in ItemFactory.java

--- src/main/java/com/sinisterorder/item/Item.java
+++ src/main/java/com/sinisterorder/item/Item.java
@@ -33,29 +33,4 @@ public class Item implements LevelHandler{
 		this.level = level;
 		this.sellValue = this.sellValue * (1 + (level / 8));
 	}
-
-    public String itemId;
-    public String itemName;
-    public int sellValue;
-    public String description;
-
-    public Item(String itemId,String itemName,int sellValue,String description){
-
-        this.itemId=itemId;
-        this.itemName=itemName;
-        this.sellValue=sellValue;
-        this.description=description;
-
-    }
-
-    public Item(){
-
-        //constructor
-
-    }
-
-     @Override
-    public String toString() {
-        return "Item{itemID='" + itemId + "', itemName='" + itemName + "', sellValue=" + sellValue + ", description='" + description + "'}";
-    }
 }
