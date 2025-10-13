e8cee669c0131dcad92108d4ae0ad6142f929190
Vicvip23 <wicekwiacek@gmail.com>
2025-10-13 13:28:53 +0200

    Implemented Consumable.java, Item.java, Weapon.java according to documentation tweaked LevelHandler.java interface to fit new architecture

--- src/main/java/com/sinisterorder/item/Item.java
+++ src/main/java/com/sinisterorder/item/Item.java
@@ -1,5 +1,37 @@
 package com.sinisterorder.item;
 
-public class Item {
+import com.sinisterorder.handler.LevelHandler;
+
+public class Item implements LevelHandler{
+	private String itemId;
+	private String itemName;
+	private int sellValue;
+	private String description;
+	private int level;
+
+	public String getId() {
+		return this.itemId;
+	}
+
+	public String getName() {
+		return this.itemName;
+	}
+
+	public int getValue() {
+		return this.sellValue;
+	}
+
+	public String getDescription(){
+		return this.description;
+	}
+
+	public int getLevel() {
+		return this.level;
+	}
+
+	public void scaleByLevel(int level) {
+		this.level = level;
+		this.sellValue = this.sellValue * (1 + (level / 8));
+	}
 
 }
--- src/main/java/com/sinisterorder/item/Weapon.java
+++ src/main/java/com/sinisterorder/item/Weapon.java
@@ -1,5 +1,24 @@
 package com.sinisterorder.item;
 
-public class Weapon {
+import java.util.ArrayList;
 
+import com.sinisterorder.attack.Attack;
+
+public class Weapon extends Item{
+	private int damage;
+	private ArrayList<Attack> attacks;
+
+	public int getDamage() {
+		return damage;
+	}
+
+	public ArrayList<Attack> getAttacks() {
+		return attacks;
+	}
+
+	@Override
+	public void scaleByLevel(int level) {
+		super.scaleByLevel(level);
+		this.damage = this.damage * (1 + (this.getLevel() / 10));
+	}
 }
