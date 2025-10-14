193ca6afafa35301694f875b6c3314173f441a9f
Bartek <bartoszwiadernylodz@gmail.com>
2025-10-14 14:29:01 +0200

    done things - added class in directory /src/main/java/attack/Attackset.java - In class added private String virable attacksetId and private Array of Strings variable attackset - also in class added getters for each one of variable

---
+++ src/main/java/com/sinisterorder/attack/Attackset.java
@@ -0,0 +1,16 @@
+package com.sinisterorder.attack;
+
+import java.util.ArrayList;
+
+public class Attackset {
+	private String attacksetId
+	private String[] attackset;
+
+	public String getAttacksetId() {
+		return this.attacksetId;
+	}
+
+	public String[] getAttackset() {
+		return attackset;
+	}
+}
