cda506a6e036dfb8209882e291c32560e5027826
Vicvip23 <wicekwiacek@gmail.com>
2025-10-26 14:20:35 +0100

    added Query test code

--- src/main/java/com/sinisterorder/app/App.java
+++ src/main/java/com/sinisterorder/app/App.java
@@ -14,6 +14,7 @@ public class App {
 		ChoiceMenu menu = MenuFactory.createFromJson("examplemenu");
 		ChoiceMenu menu2 = MenuFactory.createFromJson("examplemenu2");
 		System.out.println(menu);
+		menu2.createQuery("query1", "Specify which", "free", 0, 10);
 		menu.addAction("example1", () -> {
 			System.out.println("example one");
 			menu.setLabel("label_art", "aaa\n");
@@ -22,6 +23,8 @@ public class App {
 		});
 		menu2.addAction("example1", () -> {
 			System.out.println("here");
+			int result = menu2.query.run();
+			System.out.println(result);
 		});
 		menu2.addAction("example2", () -> {
 			menu.run();
